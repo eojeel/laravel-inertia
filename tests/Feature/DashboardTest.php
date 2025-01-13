d34dbd6e49775fc8f7173b62673af500e389d467
joe Lee <eojeel@users.noreply.github.com>
2025-01-13 19:37:02 +0000

    pest tests

---
+++ tests/Feature/DashboardTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\User;
+
+it('verified user can view dashboard', function () {
+
+    $user = User::Factory()->create();
+
+    $this->actingAs($user);
+
+    $response = $this->get(route('dashboard'));
+
+    $response->assertStatus(200);
+});
+
+it('unverified user cannot view dashboard', function () {
+
+    $user = User::Factory()->create(['email_verified_at' => null]);
+
+    $response = $this->get(route('dashboard'));
+
+    $response->assertRedirect(route('login'));
+});
