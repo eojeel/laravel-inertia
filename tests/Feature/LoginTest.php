09c08153824eff9dd22ac848cb69ec58cabb4924
joe Lee <eojeel@users.noreply.github.com>
2025-01-23 20:46:03 +0000

    52% test coverage

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -2,7 +2,12 @@
 
 use App\Models\User;
 
-use function Pest\Laravel\post;
+use function Pest\Laravel\{actingAs, get, post};
+
+it('can view the loging page', function() {
+
+    get(route('login'))->assertStatus(200);
+});
 
 it('a user can login', function () {
 
@@ -18,4 +23,25 @@ it('a user can login', function () {
     ]);
 
     $response->assertRedirect(route('dashboard'));
+});
+
+it('fails on incorrect email login attempt', function() {
+
+    $response = post('/login', [
+        'email' => fake()->email,
+        'password' => fake()->password(8)
+    ]);
+
+    $response->assertSessionHasErrors('email');
+});
+
+it('can log a user out', function () {
+
+    $user = User::factory()->create();
+
+    $response = actingAs($user)
+    ->get(route('logout'));
+    $response->assertRedirect(route('home'));
+
+    expect(Auth::user())->toBeNull();
 });
--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -1,18 +1,28 @@
 <?php
 
 use App\Models\User;
-use function Pest\Laravel\{patch, put};
 
-it('can update name and show name', function() {
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseMissing;
+
+it('can view the profile page', function () {
 
     $user = User::Factory()->create();
 
-    $this->actingAs($user);
+    actingAs($user)
+        ->get(route('profile.edit'))
+        ->assertStatus(200);
+});
 
-    patch(route('profile.update'), [
-        'name' => $name = fake()->name(),
-        'email' => $user->email,
-    ]);
+it('can update name and show updated name', function () {
+
+    $user = User::Factory()->create();
+
+    actingAs($user)
+        ->patch(route('profile.update'), [
+            'name' => $name = fake()->name(),
+            'email' => $user->email,
+        ]);
 
     $user->refresh();
 
@@ -20,35 +30,59 @@ it('can update name and show name', function() {
 
 });
 
-it('can update name and show notification', function() {
+it('can update email and resets verified email', function() {
 
     $user = User::Factory()->create();
 
-    $this->actingAs($user);
-
-    patch(route('profile.update'), [
-        'name' => $name = fake()->name(),
-        'email' => $user->email,
-    ]);
+    actingAs($user)
+        ->patch(route('profile.update'), [
+            'name' => $user->name,
+            'email' => fake()->email,
+        ]);
 
     $user->refresh();
+
+    expect($user->email_verified_at)->toBe(null);
+});
+
+it('can update name and show notification', function () {
+
+    $user = User::Factory()->create();
+
+    actingAs($user)
+        ->patch(route('profile.update'), [
+            'name' => $name = fake()->name(),
+            'email' => $user->email,
+        ]);
 
     expect(session('status'))->toBe('Profile updated.');
 
 });
 
-it('can update password and show notification', function() {
+it('can update password and show notification', function () {
 
     $user = User::Factory()->create(['password' => bcrypt('password')]);
 
-    $this->actingAs($user);
-
-    $res = put(route('profile.updatePassword'), [
-        'current_password' => 'password',
-        'new_password' => $password = fake()->password,
-        'new_password_confirmation' => $password,
-    ]);
+    actingAs($user)
+        ->put(route('profile.updatePassword'), [
+            'current_password' => 'password',
+            'new_password' => $password = fake()->password(8),
+            'new_password_confirmation' => $password,
+        ]);
 
     expect(session('status'))->toBe('Password updated.');
+
+});
+
+it('can delete a user', function() {
+
+    $user = User::Factory()->create(['password' => bcrypt('password')]);
+
+    actingAs($user)
+        ->delete(route('profile.destroy'), [
+            'password' => 'password',
+        ]);
+
+    assertDatabaseMissing('users', ['id' => $user->uid]);
 
 });
